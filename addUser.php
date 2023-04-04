42d63972be3e38db72430ac8a75cd37ba5a62d84
lyub12 <lyubmiester24@gmail.com>
2023-04-03 21:10:39 -0400

    kinda of just tired right now, feel like eh, going to  maybe take a nap

--- addUser.php
+++ addUser.php
@@ -11,4 +11,23 @@
     }
 
 
+
+    $fname = mysqli_real_escape_string($mysqli, $_POST('fname'));
+    $lname = mysqli_real_escape_string($mysqli, $_POST('lname'));
+    $email = mysqli_real_escape_string($mysqli, $_POST('email'));
+    $password = mysqli_real_escape_string($mysqli, $_POST('password'));
+
+    // Validation
+    if(strlen($fname) < 2) (
+        echo 'fname';
+    ) elseif(strlen($lname) < 2) (
+        echo 'lname';
+    ) elseif(filter_var($email) <= 4) (
+        echo 'eshort';
+    ) elseif(strlen($passwordooo) <= 4) (
+        echo 
+    )
+
+
+
 ?>
