4af0d38cb4f1d2894eb34778c1721b81437b64c0
coolistkovbel <coolistkovbel@gmail.com>
2023-04-04 15:13:13 -0400

    dumbass

--- js/register.js
+++ js/register.js
@@ -1,81 +1,67 @@
+$(document).ready(function () {
+  $("#register").click(function (e) {
+    e.preventDefault();
 
-$(document).ready(function() {
+    fname = $("#fname").val();
+    lname = $("#lname").val();
+    email = $("#email").val();
+    password = $("#password").val();
 
-    $("register").click(function (e) {
-        e.preventDefault();
-        
-
-        fname = $("#fname").val();
-        lname = $("#lname").val();
-        email = $("#email").val();
-        password = $("#password").val();
-
-
-        $.ajax({
-            type: 'POST',
-            url: "addUser.php",
-            data: `fname=${fname}&lname=${lname}&email=${email}&password=${password}`,
-            success: function (html) {
-                console.log(html)
-                if(html == 'true'){
-
-                    $("#add_err").html(`
+    $.ajax({
+      type: "POST",
+      url: "addUser.php",
+      data: `fname=${fname}&lname=${lname}&email=${email}&password=${password}`,
+      success: function (html) {
+        console.log(html);
+        if (html == "true") {
+          $("#add_err").html(`
                         <div class="alert alert-success">
                             <strong> Account </strong> procesed
                         </div>
-                    `)
+                    `);
 
-                    window.location.href = "index.php"
-
-                } else if(html == 'false') {
-                    $("#add_err").html(`
+          window.location.href = "index.php";
+        } else if (html == "false") {
+          $("#add_err").html(`
                         <div class="alret alert-danger">
                             <strong>Email address</strong> alredy in system
                         </div>
-                    `)
-                } else if(html == 'fname') {
-                    $("#add_err").html(`
+                    `);
+        } else if (html == "fname") {
+          $("#add_err").html(`
                         <div class="alret alert-danger">
                             <strong>first name</strong> alredy in system
                         </div>
-                    `)
-                } else if(html == 'lname') {
-                    $("#add_err").html(`
+                    `);
+        } else if (html == "lname") {
+          $("#add_err").html(`
                         <div class="alret alert-danger">
                             <strong>last name</strong> alredy in system
                         </div>
-                    `)
-                    
-                } else if(html == 'emailog') {
-                    $("#add_err").html(`
+                    `);
+        } else if (html == "emailog") {
+          $("#add_err").html(`
                         <div class="alret alert-danger">
                             <strong>email</strong> alredy in system
                         </div>
-                    `)
-                    
-                } else if(html == 'passwordog') {
-                    $("#add_err").html(`
+                    `);
+        } else if (html == "passwordog") {
+          $("#add_err").html(`
                         <div class="alret alert-danger">
                             <strong>password</strong> alredy in system
                         </div>
-                    `)
-                    
-                } else {
-                    $('#add_err').html(`
+                    `);
+        } else {
+          $("#add_err").html(`
                         <div class="alert alert-danger">
                             <strong>Error</strong> proccessing request
                         </div>
-                    `)
-                }
-                    
-            },
-            beforeSend: function () {
-                $("#add_err").html("loading....")
-            }
-        })
-
-
-    })    
-
-
-})
+                    `);
+        }
+      },
+      beforeSend: function () {
+        $("#add_err").html("loading....");
+      },
+    });
+  });
+});
