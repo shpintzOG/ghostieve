003544fb582e38d2391adc40f30ecc6458acb254
coolistkovbel <coolistkovbel@gmail.com>
2023-04-05 10:00:03 -0400

    going to be using sqlite3 and knex.... might actually get some poeple

---
+++ backend/data/db-config.js
@@ -0,0 +1,7 @@
+const knex = require("knex");
+
+const environment = process.env.NODE_ENV || "development";
+
+const config = require("../knexfile.js")[environment];
+
+module.exports = knex(config);
